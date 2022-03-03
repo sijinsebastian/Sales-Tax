3b33d65d97a4f181365ccc85bf1d936619f8e821
Sijin <sijinsebastian@live.com>
2022-03-03 22:54:03 +0100

    Product definition

---
+++ src/main/java/SalesTax.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalesTax {
+
+    public static List<String> createReceipt(List<String> shoppingList) {
+        List<String> receipt = new ArrayList<>();
+        return receipt;
+    }
+
+}
