ce05b125ab3c53b9a96dfadaa76fef6b18189205
Sijin <sijinsebastian@live.com>
2022-03-03 23:17:01 +0100

    Update test cases for tax calculation of imported goods. Handle the tax calculation.

--- src/main/java/TaxManager.java
+++ src/main/java/TaxManager.java
@@ -3,7 +3,25 @@ import java.util.Arrays;
 
 public class TaxManager {
 
+    private final static Float salesTaxRate = 10f;
+    private final static Float importDutyRate = 5f;
+    private final static ArrayList<String> salesTaxExemptedGoods = new ArrayList<>(Arrays.asList("book","medical", "chocolate","pills"));
+
     public static void calculateTax(Product product){
+        findSalesTax(product);
+        findImportDuty(product);
+    }
+
+    private static void findSalesTax(Product product){
+        if(!salesTaxExemptedGoods.stream().anyMatch(product.getProductDescription()::contains)){
+            product.setSalesTax(product.getBasePrice()* salesTaxRate /100);
+        }
+    }
+
+    private static void findImportDuty(Product product){
+        if (product.getIsImported()){
+            product.setImportDuty(product.getBasePrice()*importDutyRate/100);
+        }
     }
 
 
--- src/test/java/TaxManagerTest.java
+++ src/test/java/TaxManagerTest.java
@@ -19,10 +19,26 @@ class TaxManagerTest {
     }
 
     @org.junit.jupiter.api.Test
+    void taxExemptedGoodsImported() {
+        Product product = new Product("box of chocolate", 1, 11f, true);
+        TaxManager.calculateTax(product);
+        assertEquals(0f, product.getSalesTax());
+        assertEquals(0.55f, product.getImportDuty());
+    }
+
+    @org.junit.jupiter.api.Test
     void taxedGoods() {
         Product product = new Product("music CD", 1, 15f, false);
         TaxManager.calculateTax(product);
         assertEquals(1.5f, product.getSalesTax());
         assertEquals(0f, product.getImportDuty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void taxedGoodsImported() {
+        Product product = new Product("perfume", 1, 20f, true);
+        TaxManager.calculateTax(product);
+        assertEquals(2.00f, product.getSalesTax());
+        assertEquals(1.00f, product.getImportDuty());
     }
 }
